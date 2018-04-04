d73dd3c92cd2098390bb7f523f5831e764ec7c9c
Judoflip <elias.saetre@gmail.com>
2018-04-04 13:00:07 +0200

    Riktig version

--- Fiberfeil.js
+++ Fiberfeil.js
@@ -1,24 +1,103 @@
-javascript: var knr = document.getElementById('customer-info-container').getElementsByClassName('col-md-8 col-xs-8')[0].getElementsByTagName('a')[0].innerHTML;
+javascript:
+var knr = document.getElementById('customer-info-container').getElementsByClassName('col-md-8 col-xs-8')[0].getElementsByTagName('a')[0].innerHTML;
 var name = document.getElementById('customer-crm').getElementsByClassName('col-md-8 col-xs-8')[0].innerHTML;
 var kontaktInfo = document.getElementsByClassName('col-md-8 col-xs-8');
 var mobilIndex;
 var mob;
-for (i = 0; i < kontaktInfo.length; i++) {
-  if (kontaktInfo[i].innerHTML.includes("+47")) {
-    mobilIndex = i;
-  }
+
+for (i = 0; i < kontaktInfo.length; i++)
+{
+	if (kontaktInfo[i].innerHTML.includes("+47"))
+	{
+		mobilIndex = i;
+	}
+	
 }
 var mob = kontaktInfo[mobilIndex].innerHTML;
-var postnr = document.getElementById('location-details-container').getElementsByClassName('col-md-8')[1].innerHTML.replace(/\n/g, "");
-var adresse = document.getElementsByClassName('clarity-customer-address')[0].innerHTML.replace(/\n/g, "") + ", " + postnr;
-var modul = document.getElementById('access-node-config-container').getElementsByClassName('col-md-7 col-xs-12')[5].innerHTML;
-var port = document.getElementById('access-node-config-container').getElementsByClassName('col-md-7 col-xs-12')[6].innerHTML;
+
+var postnr = document.getElementById('location-details-container').getElementsByClassName('col-md-8');
+var postnrBeskrivelse = document.getElementById('location-details-container').getElementsByClassName('col-md-4');
+var postNrIndex;
+for (i = 0; i < postnr.length; i++)
+{
+	if(postnrBeskrivelse[i].innerHTML.includes("Post"))
+	{
+		postNrIndex = i;
+	}
+}
+
+var adresse = document.getElementsByClassName('clarity-customer-address')[0].innerHTML.replace(/\n/g, "") + ", " + postnr[postNrIndex].innerHTML;
+
+var portInfoBeskrivelse = document.getElementById('access-node-details-container').getElementsByClassName('col-md-5 col-xs-12');
+var portInfoSvar = document.getElementById('access-node-details-container').getElementsByClassName('col-md-7 col-xs-12');
+var portIndex;
+
+for(i = 0; i < portInfoBeskrivelse.length; i++)
+{
+	if(portInfoBeskrivelse[i].innerHTML.includes("Portstatus"))
+	{
+		portIndex = i;
+	}
+}
+
+var portStatus = portInfoSvar[portIndex].innerHTML;
+
+
 var provInfo1 = document.getElementById('access-node-config-container').getElementsByClassName('col-md-5 col-xs-12');
 var provInfo2 = document.getElementById('access-node-config-container').getElementsByClassName('col-md-7 col-xs-12');
 var infoArray = "";
-for (i = 0; i < provInfo1.length; i++) {
-  if (provInfo1[i].innerHTML.includes("Adresse") == false || provInfo1[i].innerHTML.includes("Beskrivelse") == false) {
-    infoArray += provInfo1[i].innerHTML + " " + provInfo2[i].textContent + "</br>";
-  }
+
+for (i = 0; i < provInfo1.length; i++)
+{
+	if(provInfo1[i].innerHTML.includes("Adresse") == false && provInfo1[i].innerHTML.includes("Beskrivelse") == false)
+	{
+		infoArray += provInfo1[i].innerHTML + " " + provInfo2[i].textContent + "</br>";
+	} 
 }
-window.open().document.write("Kundenummer: " + knr + "</br>" + "Navn: " + name + "</br>" + "TLF: " + mob + "</br>" + "Adresse: " + adresse + "</br></br>" + infoArray);
+
+var portInfo;
+
+var nodeNavn;
+var modulNr;
+var portNr;
+
+for(i = 0; i < provInfo1.length; i++)
+{
+	if(provInfo1[i].innerHTML.includes("Aksessnode"))
+	{
+		nodeNavn = provInfo2[i].innerHTML;
+	}
+	if(provInfo1[i].innerHTML.includes("Modulnummer"))
+	{
+		modulNr = provInfo2[i].innerHTML;
+	}
+	if(provInfo1[i].innerHTML.includes("Portnummer"))
+	{
+		portNr = provInfo2[i].innerHTML;
+	}
+}
+
+var nodeInfo = nodeNavn + " : " + modulNr + "/" + portNr;
+
+
+
+window.open().document.write("<b>Kundenummer: </b>" + knr 
+							+ "</br>"
+							+ "<b>Navn: </b>" + name 
+							+ "</br>" 
+							+ "<b>TLF: </b>" + mob 
+							+ "</br>" 
+							+ "<b>Adresse: </b>" + adresse 
+							+ "</br></br>" 
+							+ "<b>Node: </b>" + nodeInfo 
+							+ "</br></br> "
+							+ "<b>Status på port: </b>" + portStatus
+							+ "</br></br> "
+							+ "<b>Har sambandet fungert før: </b>"
+							+ "</br></br>" 
+							+ "<b>Melding: </b>"
+							+ "</br></br>" 
+							+ "<b>Portstatus fra prov: </b>" 
+							);
+							
+							
